f627c7ddd571b814c71f9b5c54af2ef38afeee84
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-08-01 17:58:22 +0300

    project added

--- RectangleDrawing/RectangleDrawing/AppDelegate.swift
+++ RectangleDrawing/RectangleDrawing/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  GesturesAndRectDrawing
+//  RectangleDrawing
 //
 //  Created by Alina Zaitseva on 8/1/18.
 //  Copyright © 2018 Alina Zaitseva. All rights reserved.
--- RectangleDrawing/RectangleDrawing/Enum/CustomColor.swift
+++
@@ -1,9 +0,0 @@
-// 
-//  CustomColor.swift
-//  GesturesAndRectDrawing
-//
-//  Created by Alina Zaitseva on 8/1/18.
-//  Copyright © 2018 Alina Zaitseva. All rights reserved.
-//
-
-import Foundation
